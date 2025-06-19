c0d53eb7a8d7479afcae86871261ae69399486c3
Athulya2511 <athulya2511@gmail.com>
2025-06-19 19:37:14 -0400

    week7

---
+++ scripts/app.js
@@ -0,0 +1,27 @@
+let fruit = {
+    name: "Apple",
+    color: "Red",
+    taste: "Sweet"
+}
+
+let name1 = `Name:${fruit.name}`
+let color =`Color:${fruit.color}`
+let taste = `Taste:${fruit.taste}`
+let fnamebyid = document.querySelector('.fruitname');
+let fcolorbyid = document.querySelector('.fruitcolor');
+let ftastebyid = document.querySelector('.fruittaste');
+
+let numberinput = document.querySelector("#numberInput");
+numberinput.addEventListener("input",function(){
+    if(numberinput.value<= -10  || numberinput.value >= 10)
+    {
+        alert("The number should be between -10 and 10");
+    }
+});
+
+console.log(fnamebyid);
+fnamebyid.innerHTML = name1;
+fcolorbyid.innerHTML = color;
+ftastebyid.innerHTML = taste;
+
+
